3a489771cc3bfce10973e49d8a38a708c2bedaae
Stefano Nepa <nepa@bryter.io>
2021-12-20 13:15:46 +0000

    initial commit

---
+++ apps/appA/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+// This starter template is using Vue 3 <script setup> SFCs
+// Check out https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <a href="/appb/">got to the Other app</a>
+  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus inventore adipisci harum? Excepturi quasi obcaecati delectus incidunt, dicta unde molestiae distinctio dolor labore placeat dignissimos laudantium quo accusantium aspernatur ab.</p>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ apps/appB/src/App.svelte
@@ -0,0 +1,27 @@
+<script>
+  import logo from './assets/svelte.png'
+  import Counter from './lib/Counter.svelte'
+</script>
+
+<main>
+  <img src={logo} alt="Svelte Logo" />
+  <h1>Hello world!</h1>
+
+    <a href="/appa/">got to the Other app</a>
+
+</main>
+
+<style>
+  :root {
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen,
+      Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+  }
+
+  main {
+    text-align: center;
+    padding: 1em;
+    margin: 0 auto;
+  }
+
+
+</style>
